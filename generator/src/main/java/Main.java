86566d4835a5faf81d56eb2a063d97d671ed930a
rawanmahdi <mahdir3@mcmaster.ca>
2023-02-21 13:12:10 -0500

    modifitications

--- generator/src/main/java/Main.java
+++ generator/src/main/java/Main.java
@@ -1,21 +1,23 @@
 import ca.mcmaster.cas.se2aa4.a2.generator.DotGen;
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
+import ca.mcmaster.cas.se2aa4.a2.io.Structs.Vertex;
+
 import ca.mcmaster.cas.se2aa4.a2.generator.wrappers.MeshM;
 import java.io.IOException;
 
 import javax.sound.sampled.SourceDataLine;
-
+import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        DotGen generator = new DotGen();
-        Mesh myMesh = generator.generate();
-        MeshFactory factory = new MeshFactory();
-        factory.write(myMesh, args[0]);
-        System.out.println("low level Mesh: ");
-        System.out.println("|Vertices| = "+ myMesh.getVerticesList().size());
-        System.out.println("|Segments| = "+ myMesh.getSegmentsList().size());
+        // DotGen generator = new DotGen();
+        // Mesh myMesh = generator.generate();
+        // MeshFactory factory = new MeshFactory();
+        // factory.write(myMesh, args[0]);
+        // System.out.println("low level Mesh: ");
+        // System.out.println("|Vertices| = "+ myMesh.getVerticesList().size());
+        // System.out.println("|Segments| = "+ myMesh.getSegmentsList().size());
         //System.out.println("|Polygons| = "+ myMesh.getPolygonsList().size())
         
         
@@ -28,15 +30,18 @@ public class Main {
         System.out.println("made polygons...");
         meshMaker.createAllCentroids();
         System.out.println("addded centroids...");
-        Mesh myMesh1 = meshMaker.buildGrid();
+        Mesh myMesh = meshMaker.buildGrid();
         System.out.println("built grid");
 
-        // MeshFactory factory1 = new MeshFactory();
-        // factory1.write(myMesh, args[0]);
+        MeshFactory factory1 = new MeshFactory();
+        factory1.write(myMesh, args[0]);
         System.out.println("Mesh ADT: ");
-        System.out.println("|Vertices| = "+ myMesh1.getVerticesList().size());
-        System.out.println("|Segments| = "+ myMesh1.getSegmentsList().size());
-        //System.out.println("|Polygons| = "+ myMesh1.getPolygonsList().size());
+        System.out.println("|Vertices| = "+ myMesh.getVerticesList().size());
+        System.out.println("|Segments| = "+ myMesh.getSegmentsList().size());
+        System.out.print("|Polygons| = ");
+        System.out.println(myMesh.getPolygonsList().size());
+        System.out.println("|Segments for first polygon in list| = " + myMesh.getPolygons(600).getSegmentIdxsCount());
+
     }
 
 }

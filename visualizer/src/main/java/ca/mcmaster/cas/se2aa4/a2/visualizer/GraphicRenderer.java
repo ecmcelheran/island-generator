c0cc2b6fa36e797dc74c0747f372f3656c5eb390
Luna Aljammal <aljammal@mcmaster.ca>
2023-02-23 16:49:11 -0500

    alpha

--- visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
+++ visualizer/src/main/java/ca/mcmaster/cas/se2aa4/a2/visualizer/GraphicRenderer.java
@@ -28,7 +28,9 @@ public class GraphicRenderer {
             double centre_x = v.getX() - (THICKNESS/2.0d);
             double centre_y = v.getY() - (THICKNESS/2.0d);
             Color old = canvas.getColor();
-            canvas.setColor(visualize? Color.BLACK : extractColor(v.getPropertiesList(), 10));
+//            canvas.setColor(visualize? Color.BLACK : extractColor(v.getPropertiesList(), 10));
+            canvas.setColor(visualize? Color.BLACK : extractColor(v.getPropertiesList()));
+
             //canvas.setColor(extractColor(v.getPropertiesList()));
             Ellipse2D point = new Ellipse2D.Double(centre_x, centre_y, THICKNESS, THICKNESS);
             canvas.fill(point);
@@ -43,7 +45,7 @@ public class GraphicRenderer {
             double x2 = vertices.get(s.getV2Idx()).getX();
             double y2 = vertices.get(s.getV2Idx()).getY();
             Color old = canvas.getColor();
-            canvas.setColor(visualize? Color.BLACK : extractColor(s.getPropertiesList(), 10));
+            canvas.setColor(visualize? Color.BLACK : extractColor(s.getPropertiesList()));
             //canvas.setColor(extractColor(s.getPropertiesList()));
             Line2D line = new Line2D.Double(x1, y1, x2, y2);
             //canvas.fill(line);
@@ -57,7 +59,7 @@ public class GraphicRenderer {
             double centre_x = vertices.get(p.getCentroidIdx()).getX() - (THICKNESS/2.0d);
             double centre_y = vertices.get(p.getCentroidIdx()).getY() - (THICKNESS/2.0d);
             Color old = canvas.getColor();
-            canvas.setColor(visualize? Color.RED : extractColor(p.getPropertiesList(), 10));
+            canvas.setColor(visualize? Color.RED : extractColor(p.getPropertiesList()));
             Ellipse2D point = new Ellipse2D.Double(centre_x, centre_y, THICKNESS, THICKNESS);
             canvas.fill(point);
             canvas.setColor(old);
@@ -73,7 +75,7 @@ public class GraphicRenderer {
                 double x2 = vertices.get(n).getX();
                 double y2 = vertices.get(n).getY();
                 Color old = canvas.getColor();
-                canvas.setColor(visualize? Color.GRAY : extractColor(p.getPropertiesList(), 10));
+                canvas.setColor(visualize? Color.GRAY : extractColor(p.getPropertiesList()));
                 Line2D line = new Line2D.Double(x1, y1, x2, y2);
                 canvas.draw(line);
                 canvas.setColor(old);
@@ -81,7 +83,7 @@ public class GraphicRenderer {
         }
     }
 
-    private Color extractColor(List<Property> properties, int i) {
+    private Color extractColor(List<Property> properties) {
         String val = null;
         for(Property p: properties) {
             if (p.getKey().equals("rgb_color")) {

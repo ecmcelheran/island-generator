347160ea7354588449a2c28eb1a43e3d7bc3ea2e
rawanmahdi <mahdir3@mcmaster.ca>
2023-02-28 08:43:37 -0500

    cropping mesh to size

--- visualizer/src/main/java/Main.java
+++ visualizer/src/main/java/Main.java
@@ -38,12 +38,14 @@ public class Main {
         String output = args[1];
         // Getting width and height for the canvas
         Structs.Mesh aMesh = new MeshFactory().read(input);
-        double max_x = Double.MIN_VALUE;
-        double max_y = Double.MIN_VALUE;
-        for (Structs.Vertex v: aMesh.getVerticesList()) {
-            max_x = (Double.compare(max_x, v.getX()) < 0? v.getX(): max_x);
-            max_y = (Double.compare(max_y, v.getY()) < 0? v.getY(): max_y);
-        }
+        // double max_x = Double.MIN_VALUE;
+        // double max_y = Double.MIN_VALUE;
+        // for (Structs.Vertex v: aMesh.getVerticesList()) {
+        //     max_x = (Double.compare(500, v.getX()) < 0? v.getX(): 500);
+        //     max_y = (Double.compare(500, v.getY()) < 0? v.getY(): 500);
+        // }
+        int max_x = 500;
+        int max_y = 500;
         
         // Creating the Canvas to draw the mesh
         Graphics2D canvas = SVGCanvas.build((int) Math.ceil(max_x), (int) Math.ceil(max_y));

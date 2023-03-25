26fec088c726832c6ed9b825313c865d0e91263e
Luna Aljammal <aljammal@mcmaster.ca>
2023-03-25 15:10:52 -0400

    F10, aquifers in low elevations

--- island/src/main/java/map/waterBodies/AquafierBuilder.java
+++ island/src/main/java/map/waterBodies/AquafierBuilder.java
@@ -1,6 +1,7 @@
 package map.waterBodies;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
@@ -14,17 +15,37 @@ public class AquafierBuilder implements WaterBuilder{
     public Map build(Mesh aMesh, Map map, int numUnits) {
         ArrayList<Structs.Polygon> border = map.getBorder();
         ArrayList<Structs.Polygon> innerLand =  map.getInnerLand();
+        HashMap<Integer, Double> elevations = map.getElevation();
+        
         Random rand = new Random();
         for(int n=0; n<numUnits; n++){
-            Structs.Polygon targetPoly = innerLand.get(rand.nextInt(innerLand.size()));
-            map.addAquafTile(targetPoly);
-            List<Integer> neighbours = targetPoly.getNeighborIdxsList();
-            for(int i: neighbours){
-                if(!border.contains(aMesh.getPolygons(i))) {
-                    map.addAquafTile(aMesh.getPolygons(i));
+            double minElevation = Double.MAX_VALUE;
+            //Structs.Polygon targetPoly = innerLand.get(rand.nextInt(innerLand.size()));
+            Structs.Polygon targetPoly = null;
+            for (Structs.Polygon polygon : innerLand) {
+                double elevation = elevations.get(aMesh.getPolygonsList().indexOf(polygon));
+                if (elevation < minElevation && !map.getAquaf().contains(polygon) ) {
+                    minElevation = elevation;
+                    targetPoly = polygon;
                 }
             }
+            if (targetPoly != null) {
+                map.addAquafTile(targetPoly);
+                map.removeLandTile(targetPoly);
+                List<Integer> neighbours = targetPoly.getNeighborIdxsList();
+                for(int i: neighbours){
+                    Structs.Polygon neighbourPoly = aMesh.getPolygons(i);
+                    if(!border.contains(neighbourPoly) && !map.getAquaf().contains(neighbourPoly)) {
+                        double neighbourElevation = elevations.get(i);
+                        if (neighbourElevation <= minElevation) {
+                      
+                          map.addAquafTile(aMesh.getPolygons(i));
+                         }
+                  }
+              }
+           }
         }
+
        return map;
     }
     

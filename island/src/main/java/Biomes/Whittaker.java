8c5611eadbf6be4b24110439908d89c17f576af7
Luna Aljammal <aljammal@mcmaster.ca>
2023-03-26 14:12:01 -0400

    f13 finalized

--- island/src/main/java/Biomes/Whittaker.java
+++ island/src/main/java/Biomes/Whittaker.java
@@ -4,26 +4,12 @@ import map.Map;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
-import map.elevation.Elevation;
-import map.elevation.FlatBuilder;
-import map.elevation.MountainBuilder;
-import map.elevation.PeakBuilder;
-import map.elevation.PlateauBuilder;
-import map.soil.Absorption;
 
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
 
 public class Whittaker {
     
-  //  private HashMap<Integer, Double> temperature ;
-    //private HashMap<Integer, Double> moisture ;
-   // private double temperature;
-    //private double moisture;
-
-
-    //public HashMap<Integer, HashMap<String, Double>> whittaker(Map map, String biome){
-        public static List<List<Double>> whittaker(int polygonIndex, Map map, String biome){
+          public static List<List<Double>> whittaker(int polygonIndex, Map map, String biome){
 
 
         HashMap<Integer,Double> absorption = map.getAbsorption();
@@ -72,141 +58,6 @@ public class Whittaker {
 
         return results;
     }
- 
-   /* public HashMap<Integer, Double> getMoisture() {
-        return moisture;
-    }
-
-
-    public HashMap<Integer, Double> getFinalTemperature(){
-        return temperature;
-    }
-*/
-    
+  
     
 }
-/*public void setElevationTemperature() {
-    for (Structs.Polygon land : this.land) {
-        List<Integer> vertices = land.getVertexList();
-        for (int vertex : vertices) {
-            double elevation = this.elevation.get(vertex);
-            double elevTemperature;
-            if (elevation > 150) {
-                elevTemperature = -7;
-            } else if (elevation > 100) {
-                elevTemperature = -2;
-            } else if (elevation > 50) {
-                elevTemperature = 5;
-            } else if (elevation > 25) {
-                elevTemperature = 10;
-            } else {
-                elevTemperature = 0;
-            }
-            this.elevation.put(vertex, elevTemperature);
-        }
-    }
-}*/
-/*
-
-package Biomes;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import map.elevation.Elevation;
-import map.elevation.FlatBuilder;
-import map.elevation.MountainBuilder;
-import map.elevation.PeakBuilder;
-import map.elevation.PlateauBuilder;
-import map.soil.Absorption;
-import map.Map;
-
-import ca.mcmaster.cas.se2aa4.a2.io.Structs;
-import ca.mcmaster.cas.se2aa4.a2.io.Structs.Mesh;
-
-public class Whittaker {
-    
-    public void whittaker(Map map, String biome){
-        HashMap<Integer,Double> absorption = map.getAbsorption();
-        HashMap<Integer,Double> elevation = map.getElevation();
-        Map genMap = new Map();
-        ArrayList<Structs.Polygon> land = genMap.getLand();
-        Mesh aMesh;
-        Structs.Mesh givenMesh = aMesh;
-
-        HashMap<Integer, String> polygonData = new HashMap<Integer, Double, Double>();
-       
-        int temperature = 0;
-        // return moisture;
-        //int temperature = Elevation.assignTemp(0)+ Biome.getTemperature();
-
-      
-       // for (Polygon p : aMesh.getLand()) {
-            int moisture = Absorption.getHumidity() + Biome.getPrecipitation();
-            String polygonValue = temperature + ":" + moisture;
-            polygonData.put(aMesh.getPolygonsList().indexOf(p), polygonValue);}
-        
-    }
-    for(Structs.Polygon land: aMesh.getLand()) {
-        Structs.Polygon.Builder pc = Structs.Polygon.newBuilder(land);
-        // if(border.contains(poly)){
-        //     color = "135,99,41";
-        // }
-        if (land.contains(land)){
-            System.out.println("land poly");
-            int elevTemperature;
-            if(elevation.get(aMesh.getLand().indexOf(land))>150){
-                elevTemperature = -7 ;
-            }
-            else if(elevation.get(aMesh.getLand().indexOf(land))>100){
-                elevTemperature = -2 ;
-            }
-            else if(elevation.get(aMesh.getLand().indexOf(land))>50){
-                elevTemperature = 5 ;
-            }
-            else if(elevation.get(aMesh.getLand().indexOf(land))>25){
-                elevTemperature = 10 ;
-            }
-            else{
-                elevTemperature = 0 ;
-
-            rgb = color.split(",");
-            blue = Integer.parseInt(rgb[2]);
-            try{
-                blue+=(absorption.get(aMesh.getPolygonsList().indexOf(poly)));
-            }catch(Exception e){
-                System.out.print("This one doesn't work");
-            }
-            if(blue>255){
-                blue = 255;
-            }
-            color = (rgb[0] + "," + rgb[1] + "," + blue);
-      
-        //else if(aquifers.contains(poly)){ // for debug
-        //     color = "0,0,0";
-        // }
-        else {
-            color = "0,0,0";
-        }
-        Structs.Property c = Structs.Property.newBuilder()
-                    .setKey("rgb_color")
-                    .setValue(color)
-                    .build();
-        pc.addProperties(c);
-        clone.addPolygons(pc);
-    }
-    return clone.build();
-}
-
-
-}
-
-
-
-return HashMap<Polygon index, final temp, final moisture>
-
-
-
-
-
-}
-*/
